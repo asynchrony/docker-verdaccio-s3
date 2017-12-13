34646b0147a536747f270b5c96bfac4de6967eda
Bruce <leeharrison1984@gmail.com>
2017-12-13 13:08:55 -0600

    Update s3-sync.sh

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/aws s3 sync /verdaccio s3://$S3_BUCKET --sse AES256 --delete
+/usr/bin/aws s3 sync /verdaccio s3://$S3_BUCKET --sse AES256 --delete --exclude storage/.sinopia-db.json
