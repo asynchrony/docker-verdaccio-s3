d6020cfcbdb6c86a7ce3089654f239a36b31fc3c
Bruce <leeharrison1984@gmail.com>
2017-12-13 10:56:58 -0600

    Create run-verdaccio.sh

---
+++ run-verdaccio.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ -n "$S3_BUCKET" ]; then
+  echo "Setting up S3-Sync"
+  chmod +x /usr/local/bin/s3-sync.sh
+  touch /var/log/s3-sync.log
+
+  echo "Doing initial sync with S3"
+  /usr/bin/aws s3 sync s3://$S3_BUCKET /var/jenkins_home
+
+  echo "*/5 * * * * /usr/local/bin/s3-sync.sh > /var/log/s3-sync.log" | /usr/bin/crontab -
+  echo "Ensure Crond is running in background"
+  /usr/sbin/crond
+fi
+
+echo "Starting Verdaccio Server"
+$APPDIR/bin/verdaccio --config /verdaccio/conf/config.yaml --listen $PROTOCOL://0.0.0.0:${PORT}
